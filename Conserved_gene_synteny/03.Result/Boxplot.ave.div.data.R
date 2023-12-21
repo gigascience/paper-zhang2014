3ccbb227213bc58cfce459f8229ce4e5213fbded
maryrosedevega <143856348+maryrosedevega@users.noreply.github.com>
2023-12-21 16:16:32 +0800

    Delete Conserved_gene_synteny directory

--- Conserved_gene_synteny/03.Result/Boxplot.ave.div.data.R
+++
@@ -1,4 +0,0 @@
-library(ggplot2)
-num <- read.table("./Both.ave.div.data",header=T)
-pdf("Boxplot.ave.div.data.pdf")
-qplot(Clade,Percentage, data=num, geom="boxplot",colour = Clade,alpha=I(1/100),size =I(1.2),ylab="Percentage") + theme(axis.line=element_line()) + theme(panel.background = element_blank()) + theme(panel.grid.major = element_blank()) + theme(panel.grid.minor = element_blank()) 
